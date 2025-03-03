2076badb1ad4a61097fd118df39564179b9764f1
jenyaftw <yyakovliev02@gmail.com>
2025-03-03 17:22:52 +0200

    opencomposite-2

--- pkgs/opencomposite/default.nix
+++ pkgs/opencomposite/default.nix
@@ -6,6 +6,6 @@ opencomposite.overrideAttrs (oldAttrs: {
     repo = "OpenOVR";
     fetchSubmodules = true;
     rev = "578a1a5c57204e7db31299351c8e312ead9ba7c4";
-    hash = lib.fakeHash;
+    hash = "sha256-LTC/RJdi4saj5hVli23GUm+ytKPv/Z94WHlo5s+tMeY=";
   };
 })
